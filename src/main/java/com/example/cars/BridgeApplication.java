9b8b5e641d8ea9cbd5e925099c20c7c0d77e70bc
maisushii <maissa.hermessi@esprit.tn>
2025-05-18 16:10:14 +0100

    zedt module notif

--- src/main/java/com/example/cars/BridgeApplication.java
+++ src/main/java/com/example/cars/BridgeApplication.java
@@ -1,5 +1,6 @@
 package com.example.cars;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,7 +10,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class BridgeApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables
+		Dotenv dotenv = Dotenv.load();
+
+		// Example: access a variable
+		String sid = dotenv.get("TWILIO_ACCOUNT_SID");
+		System.out.println("TWILIO SID: " + sid); // Optional debug line
+
 		SpringApplication.run(BridgeApplication.class, args);
 	}
-
 }
