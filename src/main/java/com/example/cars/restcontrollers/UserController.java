985a25280b84b8fd38f3ebdd9161b63333716e8d
Mahdi <mahdi.zaied@istic.ucar.tn>
2025-05-27 21:47:48 +0100

    auth done

--- src/main/java/com/example/cars/restcontrollers/UserController.java
+++ src/main/java/com/example/cars/restcontrollers/UserController.java
@@ -1,13 +1,26 @@
 package com.example.cars.restcontrollers;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.cars.entities.Nageur;
+import com.example.cars.entities.User;
+import com.example.cars.services.UserService;
+
+
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("api/v1/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
 
+    private final UserService service ;
+
+@GetMapping
+    public List<User> getUsers() {    return service.users();}
 
 }
