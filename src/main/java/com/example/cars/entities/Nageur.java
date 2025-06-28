534eb110394f04102bc047cc9266b8389bc7aaa5
Eyaa54 <148654571+Eyaa54@users.noreply.github.com>
2025-06-28 10:04:07 +0100

    Update Nageur.java

--- src/main/java/com/example/cars/entities/Nageur.java
+++ src/main/java/com/example/cars/entities/Nageur.java
@@ -1,10 +1,11 @@
 package com.example.cars.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -16,25 +17,41 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Nageur {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
-    private String nom;
-    private String prenom;
-    private int age;
-    private String sexe;
-    private String email;
-    private String telephone;
-    private String niveau; // Beginner, Intermediate, Advanced, etc.
+    // Correspond à firstName dans TS
+    String firstName;
 
+    // Correspond à lastName dans TS
+    String lastName;
+
+    int age;
+
+    // Correspond à gender dans TS
+    String gender;
+
+    String phone;
+
+    String email;
+
+    String category;
+
+    String level;
+
+    // Correspond à sessionsPerWeek dans TS
+    int sessionsPerWeek;
+    Boolean   actif;
+
+    // Correspond à registrationDate dans TS
+    LocalDate registrationDate;
     @OneToMany(mappedBy = "nageur")
+    @JsonManagedReference
     private List<EvaluationSante> evaluations;
 
     @OneToMany(mappedBy = "nageur")
+    @JsonManagedReference
     private List<Reservation> reservations;
-
-    @JsonIgnoreProperties("nageurs")
-    @ManyToMany(mappedBy = "nageurs")
-    private List<Groupe> groupes;
 }
