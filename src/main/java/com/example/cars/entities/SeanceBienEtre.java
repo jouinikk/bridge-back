33a2bea94314c29a6c721403df29631a568257a7
Eyaa54 <148654571+Eyaa54@users.noreply.github.com>
2025-06-28 10:01:10 +0100

    Update SeanceBienEtre.java

--- src/main/java/com/example/cars/entities/SeanceBienEtre.java
+++ src/main/java/com/example/cars/entities/SeanceBienEtre.java
@@ -6,27 +6,39 @@ import lombok.experimental.FieldDefaults;
 import java.util.Date;
 import java.util.List;
 
-
 @Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SeanceBienEtre {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Temporal(TemporalType.TIMESTAMP)
     private Date date;
 
     private int capacite;
 
+    private Integer duree; // Durée en minutes
+
+    private int placesDisponibles;
+
+    private String instructeur;
+
+    private String emailInstructeur;
+
+    private String niveau;
+    Boolean   actif;
+
     @Enumerated(EnumType.STRING)
     private TypeSeance type;
-    @OneToMany(mappedBy = "seanceBienEtre")
+
+    @OneToMany(mappedBy = "seanceBienEtre", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Reservation> reservations;
 }
